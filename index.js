c8c385e0d857b700094ab12e55a4613bc440ba9b
Glauber <glauber.bannwart@gmail.com>
2023-05-25 18:55:13 -0300

    Speakers Text Update (Customization)

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-list');
 const close = document.querySelector('.close');
 
-
 close.addEventListener('click', () => {
   nav.classList.toggle('active');
   close.classList.toggle('active');
@@ -23,50 +22,50 @@ const speakers = [
     title: 'Ex-Astronaut and TED Speaker',
     description: "Motivation is what drives change in the world. Wrote several books like 'From Space to earth', 'How Team's can outperform their Status Quo' and 'The origin's of EQ: 360o Emotional Inteligence', motivational speaker and Master Coach.",
     image: 'speaker_01.png',
-    linkedin: 'linkedin.com'
+    linkedin: 'linkedin.com',
   },
   {
     name: 'Guntler Honda',
     title: 'CEO at Hawk Farm Operations',
     description: 'Association for Farm Cooperatives, that uses tecnology to build a viable and sustainable ecossistem. Contribution to SDG 2030 and other initiatives.',
     image: 'speaker_02.png',
-    linkedin: 'linkedin.com'
+    linkedin: 'linkedin.com',
   },
   {
     name: 'Koi Zanty',
     title: 'Professor at Beijing University',
     description: 'Focusing on a collaborative approach in a networked environment, we created the concept of co-production based on sharing with open source software and Wikipedia. (Major books: The Green Frontier from 2020 to 2030)',
     image: 'speaker_03.png',
-    linkedin: 'linkedin.com'
+    linkedin: 'linkedin.com',
   },
   {
     name: 'Oxoki Humbert',
     title: 'PhD, Professor at South African University',
-    description: 'Focusing on a collaborative approach in a networked environment, we created the concept of co-production based on sharing with open source software and Wikipedia. (Major books: The Farm Wealth, People and Animals)',
+    description: "Extensive reaseach on Animals on the African Continent. Books: African Mamals, Relations of Insects with Plants, Rooiboos Tea and why it can't be cultivated outside of South Africa",
     image: 'speaker_04.png',
-    linkedin: 'linkedin.com'
+    linkedin: 'linkedin.com',
   },
   {
     name: 'Johanna Betley',
     title: 'Counsel Member at MIT',
     description: 'Focusing on Tecnology use in farming. How automation can improve the management costs and long term proftability for a better future.',
     image: 'speaker_05.png',
-    linkedin: 'linkedin.com'
+    linkedin: 'linkedin.com',
   },
   {
     name: 'John Witt',
     title: 'Professor at Harvard Soil Studies',
     description: 'Focusing on a collaborative approach in a networked environment, we created the concept of co-production based on sharing with open source software and Wikipedia. (Major books: The Wealth of the Network, Penguins and Leviathan)',
     image: 'speaker_06.png',
-    linkedin: 'linkedin.com'
-  }
+    linkedin: 'linkedin.com',
+  },
 ];
 
 const speakerSection = document.querySelector('.speakersection');
 
 const numParents = Math.ceil(speakers.length / 2);
 
-for (let i = 1; i <= numParents; i++) {
+for (let i = 1; i <= numParents; i + 1) {
   const speakerParent = document.createElement('div');
   speakerParent.classList.add(`speakerparent${i}`);
   speakerSection.appendChild(speakerParent);
@@ -110,6 +109,8 @@ speakers.forEach((speaker, index) => {
   speakerCard.appendChild(speakerHeader);
   speakerParent.appendChild(speakerCard);
 });
+
+// Creation of the button and some <br> tags
 
 const moreButton = document.createElement('button');
 moreButton.classList.add('btnspeaker');
